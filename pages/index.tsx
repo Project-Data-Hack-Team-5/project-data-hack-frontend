c5505c02b53e4336928b712ef5fee3a4994643db
Yusuf <yusufcodes@outlook.com>
2022-10-21 15:13:37 +0100

    Make a call to API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+
+const axios = require('axios');
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    (async () => {
+      const response = await axios.post(
+        'https://mw4ksab47k.execute-api.eu-west-1.amazonaws.com/prod/hello',
+        {
+          name: 'Yusuf Website'
+        }
+      );
+      console.log(response.data);
+    })();
+  }, []);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
